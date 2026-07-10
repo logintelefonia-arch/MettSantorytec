61aa9548725b3aaa324b7c4962c6c43bcfe63537
Vinhal <logintelefonia@gmail.com>
2026-07-10 14:36:41 -0300

    Add files via upload

---
+++ backup-data.js
@@ -0,0 +1,12 @@
+window.CONTROLE_CONVENIOS_BACKUP = {
+  id: "controle-convenios-vazio-1",
+  activePayer: "unimed",
+  payers: {
+    unimed: { registrations: [], protocols: [] },
+    caesan: { registrations: [], protocols: [] },
+    marinha: { registrations: [], protocols: [] },
+    "cirurgia-segura": { registrations: [], protocols: [] },
+    assefaz: { registrations: [], protocols: [] },
+    "postal-saude": { registrations: [], protocols: [] },
+  },
+};
